b9f7c7c9b6e4a07f06ec0fac0abff6d8b9d35394
Ian Lai <i3lai@edu.uwaterloo.ca>
2017-05-07 22:58:38 -0400

    Attempting to integrate Google Analytics

--- scripts/main.js
+++ scripts/main.js
@@ -44,4 +44,12 @@ window.onload = function(){
 	canvas.setAttribute("height",window.innerHeight);
 	canvas.setAttribute("width",window.innerWidth);
 	clearCanvas();
+
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-98672458-1', 'auto');
+	  ga('send', 'pageview');
 };
